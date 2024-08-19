ed4b81b07b5eea1075169b48256b7b2d0483954c
Ilya <splint1777@gmail.com>
2024-08-19 18:54:28 +0300

    add constants

---
+++ js/constants/index.js
@@ -0,0 +1,10 @@
+// import { Rook } from "../figures/index.js";
+// import { Rook } from "../figures/Rook/index.js";
+
+// console.log(Rook);
+
+
+export const LETTERS = Array.from('abcdefgh');
+
+
+
